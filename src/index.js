c99e778fe395b1620dc01b90f0bc79c87ad788bf
HM <haimeng_ll@163.com>
2020-12-14 22:08:39 +0800

    登录静态页面

--- src/index.js
+++ src/index.js
@@ -1,5 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
+// 由于 antd 组件的默认文案是英文，所以需要修改为中文
+// import zhCN from "antd/lib/locale/zh_CN";
+import moment from "moment";
+// import "moment/locale/zh-cn";
+import "antd/dist/antd.css";
 
 ReactDOM.render(<App />, document.getElementById("root"));
